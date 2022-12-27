8be1cb10eab4ca26c675ed9d1835bd32da557fda
keamy-eun <eun.wonki014@gmail.com>
2022-12-27 10:08:54 +0900

    -

---
+++ src/main/java/com/keamy/study_servlets/filters/SessionsFilter.java
@@ -0,0 +1,39 @@
+package com.keamy.study_servlets.filters;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebFilter("/session/*")
+public class SessionsFilter implements Filter{
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        System.out.println(request.getRemoteHost());
+        System.out.println(request.getRemoteAddr());
+        HttpServletRequest httpServletRequest =  (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+
+        String uri = httpServletRequest.getRequestURI();
+
+        chain.doFilter(request, response); // chain을 걸어야 웹이 연결된다
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        
+    }
+}
---
+++ src/main/java/com/keamy/study_servlets/filters/SimpleFilter.java
@@ -0,0 +1,32 @@
+package com.keamy.study_servlets.filters;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+
+@WebFilter("/*")
+public class SimpleFilter implements Filter{
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        System.out.println(request.getRemoteHost());
+        System.out.println(request.getRemoteAddr());
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        
+    }
+}
